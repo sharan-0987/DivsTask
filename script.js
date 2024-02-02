77871a5ae800d3460245899457002d5296ef082c
Sharan Abishek <85361434+sharan-0987@users.noreply.github.com>
2024-02-02 13:49:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// var division = document.createElement("div");
+
+// var diva = document.createElement("div");
+// division.appendChild(diva);
+
+// var divb = document.createElement("div");
+// division.appendChild(divb);
+
+// var divc = document.createElement("div");
+// division.appendChild(divc);
+
+// var divd = document.createElement("div");
+// division.appendChild(divd);
+
+// var H1 = document.createElement("h1");
+// H1.textContent = "header";
+// diva.appendChild(H1);
+
+// var H2 = document.createElement("h1");
+// H2.textContent = "main";
+// divb.appendChild(H2);
+
+// var H3 = document.createElement("h1");
+// H3.textContent = "sidebar";
+// di
+
+// document.body.appendChild(division);
+
